785d4496024fccb2b609b3091229ca5dfb519b5b
vaclavsvoboda-net <vaclavsvoboda.net@gmail.com>
2022-08-12 07:59:43 +0200

    Přidejte soubory projektu.

---
+++ RPiPS2Com/RPiPS2Com.cs
@@ -0,0 +1,278 @@
+﻿using System.Device.Gpio;
+using System.Diagnostics;
+
+namespace RPiPS2Comunication
+{
+    public class RPiPS2Com
+    {
+        public string LastExceptionMessage = "";
+
+        private int clockPin;
+        private int dataPin;
+
+        private long ticksPerPulse = Stopwatch.Frequency / 1000000L * 40L; // 40us per pulse
+        private long ticksPerPulseHalf = (Stopwatch.Frequency / 1000000L * 40L) / 2; // 20us per half of pulse
+        private long ticksWaitAfterByte = Stopwatch.Frequency / 1000000L * 1000L;
+
+        private GpioController controller = new GpioController();
+
+        public RPiPS2Com(int ClockPin, int DataPin)
+        {
+            clockPin = ClockPin;
+            dataPin = DataPin;
+        }
+        public bool Initialize()
+        {
+            if (controller.IsPinOpen(clockPin) || controller.IsPinOpen(dataPin))
+            {
+                LastExceptionMessage = "Pins can not be open.";
+                return false;
+            }
+            if (!controller.IsPinModeSupported(clockPin, PinMode.InputPullUp) || !controller.IsPinModeSupported(dataPin, PinMode.InputPullUp))
+            {
+                LastExceptionMessage = "Pins can not be set as input.";
+                return false;
+            }
+            if (!controller.IsPinModeSupported(clockPin, PinMode.Output) || !controller.IsPinModeSupported(dataPin, PinMode.Output))
+            {
+                LastExceptionMessage = "Pins can not be set as output.";
+                return false;
+            }
+
+            controller.OpenPin(clockPin);
+            controller.OpenPin(dataPin);
+
+            SetHi(clockPin);
+            SetHi(dataPin);
+
+            return true;
+        }
+        public void PressButton(Buttons value)
+        {
+            Write(((byte)value));
+        }
+        public void ReleaseButton(Buttons value)
+        {
+            Write(0xF0);
+            Write(((byte)value));
+        }
+        public void PressAndReleaseButton(Buttons value)
+        {
+            Write(((byte)value));
+            Write(0xF0);
+            Write(((byte)value));
+        }
+        public void WriteString(string value)
+        {
+            foreach (char ch in value)
+            {
+                char chUpper = Char.ToUpper(ch);
+                Buttons button = GetButton(chUpper);
+                if (button != 0)
+                {
+                    if (Char.IsUpper(ch))
+                    {
+                        PressButton(Buttons.LEFT_SHIFT);
+                        PressAndReleaseButton(button);
+                        ReleaseButton(Buttons.LEFT_SHIFT);
+                    }
+                    else
+                    {
+                        PressAndReleaseButton(button);
+                    }
+                }
+            }
+        }
+
+        private bool Write(byte value)
+        {
+            if (controller.Read(clockPin) == PinValue.Low || controller.Read(dataPin) == PinValue.Low)
+            {
+                LastExceptionMessage = "Can not write, clock or data are LOW.";
+                return false;
+            }
+
+            byte countOf1s = 0;
+
+            // Start bit
+            long ticksRemainToPulseEnd = SetLow(dataPin);
+            WaitTicks(ticksRemainToPulseEnd - ticksPerPulseHalf);
+            ticksRemainToPulseEnd = SetLow(clockPin);
+            WaitTicks(ticksRemainToPulseEnd);
+            ticksRemainToPulseEnd = SetHi(clockPin);
+            WaitTicks(ticksRemainToPulseEnd - ticksPerPulseHalf);
+
+            // Data
+            for (int i = 0; i < 8; i++)
+            {
+                if ((value & 1) == 1)
+                {
+                    countOf1s++;
+                    ticksRemainToPulseEnd = SetHi(dataPin);
+                }
+                else
+                {
+                    ticksRemainToPulseEnd = SetLow(dataPin);
+                }
+                WaitTicks(ticksRemainToPulseEnd - ticksPerPulseHalf);
+                ticksRemainToPulseEnd = SetLow(clockPin);
+                WaitTicks(ticksRemainToPulseEnd);
+                ticksRemainToPulseEnd = SetHi(clockPin);
+                WaitTicks(ticksRemainToPulseEnd - ticksPerPulseHalf);
+
+                value >>= 1;
+            }
+
+            // Parity
+            if (countOf1s % 2 == 0)
+            {
+                ticksRemainToPulseEnd = SetHi(dataPin);
+            }
+            else
+            {
+                ticksRemainToPulseEnd = SetLow(dataPin);
+            }
+            WaitTicks(ticksRemainToPulseEnd - ticksPerPulseHalf);
+            ticksRemainToPulseEnd = SetLow(clockPin);
+            WaitTicks(ticksRemainToPulseEnd);
+            ticksRemainToPulseEnd = SetHi(clockPin);
+            WaitTicks(ticksRemainToPulseEnd - ticksPerPulseHalf);
+
+            // Stop
+            ticksRemainToPulseEnd = SetHi(dataPin);
+            WaitTicks(ticksRemainToPulseEnd - ticksPerPulseHalf);
+            ticksRemainToPulseEnd = SetLow(clockPin);
+            WaitTicks(ticksRemainToPulseEnd);
+            ticksRemainToPulseEnd = SetHi(clockPin);
+            WaitTicks(ticksRemainToPulseEnd - ticksPerPulseHalf);
+
+            WaitTicks(ticksWaitAfterByte);
+
+            return true;
+        }
+        private long SetHi(int pin)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            controller.SetPinMode(pin, PinMode.InputPullUp);
+            controller.Write(pin, PinValue.High);
+            stopwatch.Stop();
+            return ticksPerPulse - stopwatch.ElapsedTicks;
+        }
+        private long SetLow(int pin)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            controller.SetPinMode(pin, PinMode.Output);
+            controller.Write(pin, PinValue.Low);
+            stopwatch.Stop();
+            return ticksPerPulse - stopwatch.ElapsedTicks;
+        }
+        private void WaitTicks(long ticks)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            while (stopwatch.ElapsedTicks < ticks) { }
+        }
+        private Buttons GetButton(char value)
+        {
+            if (Enum.TryParse(typeof(Buttons), value.ToString(), out object? newValue))
+            {
+                return newValue == null ? 0 : (Buttons)newValue;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public enum Buttons : byte
+        {
+            ESCAPE = 0x76,
+            F1 = 0x05,
+            F2 = 0x06,
+            F3 = 0x04,
+            F4 = 0x0c,
+            F5 = 0x03,
+            F6 = 0x0b,
+            F7 = 0x83,
+            F8 = 0x0a,
+            F9 = 0x01,
+            F10 = 0x09,
+            F11 = 0x78,
+            F12 = 0x07,
+            SCROLL_LOCK = 0x7e,
+            ACCENT = 0x0e,
+            ONE = 0x16,
+            TWO = 0x1e,
+            THREE = 0x26,
+            FOUR = 0x25,
+            FIVE = 0x2e,
+            SIX = 0x36,
+            SEVEN = 0x3d,
+            EIGHT = 0x3e,
+            NINE = 0x46,
+            ZERO = 0x45,
+            MINUS = 0x4e,
+            EQUAL = 0x55,
+            BACKSPACE = 0x66,
+            TAB = 0x0d,
+            Q = 0x15,
+            W = 0x1d,
+            E = 0x24,
+            R = 0x2d,
+            T = 0x2c,
+            Y = 0x35,
+            U = 0x3c,
+            I = 0x43,
+            O = 0x44,
+            P = 0x4d,
+            OPEN_BRACKET = 0x54,
+            CLOSE_BRACKET = 0x5b,
+            BACKSLASH = 0x5d,
+            CAPS_LOCK = 0x58,
+            A = 0x1c,
+            S = 0x1b,
+            D = 0x23,
+            F = 0x2b,
+            G = 0x34,
+            H = 0x33,
+            J = 0x3b,
+            K = 0x42,
+            L = 0x4b,
+            SEMI_COLON = 0x4c,
+            TICK_MARK = 0x52,
+            ENTER = 0x5a,
+            LEFT_SHIFT = 0x12,
+            Z = 0x1a,
+            X = 0x22,
+            C = 0x21,
+            V = 0x2a,
+            B = 0x32,
+            N = 0x31,
+            M = 0x3a,
+            COMMA = 0x41,
+            PERIOD = 0x49,
+            SLASH = 0x4a,
+            RIGHT_SHIFT = 0x59,
+            LEFT_CONTROL = 0x14,
+            LEFT_ALT = 0x11,
+            SPACE = 0x29,
+            NUM_LOCK = 0x77,
+            ASTERISK = 0x7c,
+            NUMPAD_MINUS = 0x7b,
+            NUMPAD_SEVEN = 0x6c,
+            NUMPAD_EIGHT = 0x75,
+            NUMPAD_NINE = 0x7d,
+            PLUS = 0x79,
+            NUMPAD_FOUR = 0x6b,
+            NUMPAD_FIVE = 0x73,
+            NUMPAD_SIX = 0x74,
+            NUMPAD_ONE = 0x69,
+            NUMPAD_TWO = 0x72,
+            NUMPAD_THREE = 0x7a,
+            NUMPAD_ZERO = 0x70,
+            DECIMAL = 0x71
+        }
+    }
+}
---
+++ RPiPS2Keyboard/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Net;
+using System.Device.Gpio;
+using RPiPS2Comunication;
+using System.Diagnostics;
+
+namespace RPiPS2Keyboard
+{
+    internal class Program
+    {
+        private static RPiPS2Com RPiPS2Com = new(24, 23);
+        private static GpioController Controller = new();
+        private static System.Net.Sockets.UdpClient UdpClient = new(new IPEndPoint(IPAddress.Any, 54321));
+
+        private static byte KeyboardId = 0;
+
+        private static byte PinAppRunLED = 25;
+        private static byte PinNetworkLED = 7;
+        private static byte PinTestButton = 21;
+        private static byte PinKeyboard1 = 5;
+        private static byte PinKeyboard2 = 12;
+        private static byte PinKeyboard3 = 13;
+        private static byte PinKeyboard4 = 16;
+
+        private static bool AppRunLED = false;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"App started at: {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+
+            if (!RPiPS2Com.Initialize())
+            {
+                Console.Write(RPiPS2Com.LastExceptionMessage);
+                return;
+            }
+
+            // App running LED
+            if (!Controller.IsPinModeSupported(PinAppRunLED, PinMode.Output))
+            {
+                Console.WriteLine($"Gpio pin {PinAppRunLED} can not bo set.");
+                return;
+            }
+            else
+            {
+                Controller.OpenPin(PinAppRunLED, PinMode.Output);
+                Controller.Write(PinAppRunLED, PinValue.Low);
+            }
+
+            // Network LED
+            if (!Controller.IsPinModeSupported(PinNetworkLED, PinMode.Output))
+            {
+                Console.WriteLine($"Gpio pin {PinNetworkLED} can not bo set.");
+                return;
+            }
+            else
+            {
+                Controller.OpenPin(PinNetworkLED, PinMode.Output);
+                Controller.Write(PinNetworkLED, PinValue.Low);
+            }
+
+            // Test button
+            if (!Controller.IsPinModeSupported(PinTestButton, PinMode.InputPullUp))
+            {
+                Console.WriteLine($"Gpio pin {PinTestButton} can not bo set.");
+                return;
+            }
+            else
+            {
+                Controller.OpenPin(PinTestButton, PinMode.InputPullUp);
+            }
+
+            // Keyboard 1 jumper
+            if (!Controller.IsPinModeSupported(PinKeyboard1, PinMode.InputPullDown))
+            {
+                Console.WriteLine($"Gpio pin {PinKeyboard1} can not bo set.");
+                return;
+            }
+            else
+            {
+                Controller.OpenPin(PinKeyboard1, PinMode.InputPullUp);
+            }
+
+            // Keyboard 2 jumper
+            if (!Controller.IsPinModeSupported(PinKeyboard2, PinMode.InputPullDown))
+            {
+                Console.WriteLine($"Gpio pin {PinKeyboard2} can not bo set.");
+                return;
+            }
+            else
+            {
+                Controller.OpenPin(PinKeyboard2, PinMode.InputPullUp);
+            }
+
+            // Keyboard 3 jumper
+            if (!Controller.IsPinModeSupported(PinKeyboard3, PinMode.InputPullDown)) 
+            {
+                Console.WriteLine($"Gpio pin {PinKeyboard3} can not bo set.");
+                return;
+            }
+            else
+            {
+                Controller.OpenPin(PinKeyboard3, PinMode.InputPullUp);
+            }
+
+            // Keyboard 4 jumper
+            if (!Controller.IsPinModeSupported(PinKeyboard4, PinMode.InputPullDown)) 
+            {
+                Console.WriteLine($"Gpio pin {PinKeyboard4} can not bo set.");
+                return;
+            }
+            else
+            {
+                Controller.OpenPin(PinKeyboard4, PinMode.InputPullUp);
+            }
+
+            if (Controller.Read(PinKeyboard1) == PinValue.Low) KeyboardId = 1;
+            if (Controller.Read(PinKeyboard2) == PinValue.Low) KeyboardId = 2;
+            if (Controller.Read(PinKeyboard3) == PinValue.Low) KeyboardId = 3;
+            if (Controller.Read(PinKeyboard4) == PinValue.Low) KeyboardId = 4;
+
+            Stopwatch stopwatch1000ms = new();
+            stopwatch1000ms.Start();
+            Stopwatch stopwatch100ms = new();
+            stopwatch100ms.Start();
+
+            while (true) 
+            {
+                if (stopwatch1000ms.ElapsedMilliseconds > 1000)
+                {
+                    AppRunLED = !AppRunLED;
+                    Controller.Write(PinAppRunLED, AppRunLED ? PinValue.High : PinValue.Low);
+                    Controller.Write(PinNetworkLED, System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() ? PinValue.High : PinValue.Low);
+                    stopwatch1000ms.Restart();
+                }
+                if (stopwatch100ms.ElapsedMilliseconds > 100)
+                {
+                    if (Controller.Read(PinTestButton) == PinValue.Low)
+                    {
+                        RPiPS2Com.PressAndReleaseButton(RPiPS2Com.Buttons.A);
+                        RPiPS2Com.PressAndReleaseButton(RPiPS2Com.Buttons.ENTER);
+                        while (Controller.Read(PinTestButton) == PinValue.Low) { }
+                    }
+                    if (UdpClient.Available > 0)
+                    {
+                        byte[] buffer = new byte[UdpClient.Available];
+                        UdpClient.Client.Receive(buffer);
+                        string receivedString = System.Text.Encoding.UTF8.GetString(buffer);
+                        if (receivedString.StartsWith("RPiPS2Kbd"))
+                        {
+                            string[] data = receivedString.Split('|');
+                            if (data.Length == 3)
+                            {
+                                if (byte.TryParse(data[1], out byte receivedId))
+                                {
+                                    if (receivedId == KeyboardId)
+                                    {
+                                        RPiPS2Com.WriteString(data[2]);
+                                        RPiPS2Com.PressAndReleaseButton(RPiPS2Com.Buttons.ENTER);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    stopwatch100ms.Restart();
+                }
+            }
+        }
+    }
+}
